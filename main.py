3ca798e90db63e09a5cec054d886056a1cc78066
Xuan Huy NGUYEN <nxhuy2611@gmail.com>
2022-09-17 22:11:40 +0200

    schema validation: pydantic - default value and Optional

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -7,6 +8,8 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -17,6 +20,7 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post.title)
-    return {"data": new_post}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}

d812714478531cbfd197d1d9bad1787b3324c6b0
Xuan Huy NGUYEN <nxhuy2611@gmail.com>
2022-09-17 21:50:01 +0200

    POST request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,6 +7,11 @@ app = FastAPI()
 async def root():
     return {"message": "Welcome to my api"}
 
-@app.get("/")
+@app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": payload}

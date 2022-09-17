8ad8a6e5e76a54e9590e32205af97e0129c01fd6
Xuan Huy NGUYEN <nxhuy2611@gmail.com>
2022-09-17 22:04:12 +0200

    schema validation: pydantic

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def root():
@@ -12,6 +17,6 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": payload}
+def create_posts(new_post: Post):
+    print(new_post.title)
+    return {"data": new_post}
